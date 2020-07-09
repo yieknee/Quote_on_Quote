07860f6e21b26e17be70c7fae82d4828444057ea
Yieni <yaynee8d@gmail.com>
2020-07-08 20:28:37 -0700

    added firebase to android studio

--- app/src/main/java/com/example/quote_on_quote/MainActivity.java
+++ app/src/main/java/com/example/quote_on_quote/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.quote_on_quote;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
