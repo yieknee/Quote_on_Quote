ba5fb7a2a7a33176272e5d2d1cf898aa6d2bf550
Yieni <yaynee8d@gmail.com>
2020-07-09 19:49:27 -0700

    write scarfold for methods I will create

--- app/src/main/java/com/example/quote_on_quote/MainActivity.java
+++ app/src/main/java/com/example/quote_on_quote/MainActivity.java
@@ -10,4 +10,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //TODO: seed my database with 20 question objects.
+    //TODO: make calls to my database to generate the set of questions the player will answer.
+
+
+    public int countCorrect(){
+        //this will count how many questions the player got correct and return the count
+    }
+
+    public String gameOver(int numberCOrrect, int numberOfQuestions){
+        // this will output some type of message  to the player that tells them if they won or lost
+    }
+
 }
