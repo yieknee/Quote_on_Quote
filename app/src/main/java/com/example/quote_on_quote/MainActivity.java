1e9603ea505f88c50db68bf2e38d00e687e37647
Yieni <yaynee8d@gmail.com>
2020-07-18 15:37:07 -0700

    fix bug that let player submit without choosing answer by disabling submit button until player chooses ansewr

--- app/src/main/java/com/example/quote_on_quote/MainActivity.java
+++ app/src/main/java/com/example/quote_on_quote/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.quote_on_quote;
 
 import androidx.appcompat.app.AlertDialog;
+
 import android.content.DialogInterface;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
@@ -17,140 +20,154 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    ArrayList<Question> gameQuestions;
-    int totalCorrectAnswers;
-    int totalQuestions;
-    int playerAnswer;
-    int currentQuestionIndex = -1;
-    int correctAnswerButtonId;
+  ArrayList<Question> gameQuestions;
+  int totalCorrectAnswers = 0;
+  int totalQuestions;
+  int playerAnswer;
+  int currentQuestionIndex = -1;
+  int correctAnswerButtonId;
 
-    ImageView quoteImageView;
-    TextView questionTextView;
-    TextView answerResultText;
-    Button playButton;
-    Button answer0Button;
-    Button answer1Button;
-    Button answer2Button;
-    Button answer3Button;
-    Button submitButton;
-    Button moreInfoButton;
-    Button nextQuestionButton;
-    Button correctAnswerButton;
-    LinearLayout buttonHolder;
+  ImageView quoteImageView;
+  TextView questionTextView;
+  TextView answerResultText;
+  Button playButton;
+  Button answer0Button;
+  Button answer1Button;
+  Button answer2Button;
+  Button answer3Button;
+  Button submitButton;
+  Button moreInfoButton;
+  Button nextQuestionButton;
+  Button correctAnswerButton;
+  LinearLayout buttonHolder;
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.landing_page_main);
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.landing_page_main);
 
         /*(6) Had to set the play button by default to be unclickable until all the questions for the game get loaded.
         Visually signalled the play button being inactive by making the color gray.
         ** see landing_page_main xml file
          */
-        playButton = findViewById(R.id.playbutton);
+    playButton = findViewById(R.id.playbutton);
 
-        playButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                setContentView(R.layout.game_play_main);
-                quoteImageView = findViewById(R.id.quoteimg);
-                questionTextView = findViewById(R.id.question);
-                answerResultText = findViewById(R.id.answerresult);
-                answer0Button = findViewById(R.id.answer0);
-                answer1Button = findViewById(R.id.answer1);
-                answer2Button = findViewById(R.id.answer2);
-                answer3Button = findViewById(R.id.answer3);
-                submitButton = findViewById(R.id.submit);
-                moreInfoButton = findViewById(R.id.moreinfo);
-                nextQuestionButton = findViewById(R.id.nextquestion);
-                buttonHolder = findViewById(R.id.buttonholder);
+    playButton.setOnClickListener(new View.OnClickListener() {
+      public void onClick(View v) {
+        setContentView(R.layout.game_play_main);
+        quoteImageView = findViewById(R.id.quoteimg);
+        questionTextView = findViewById(R.id.question);
+        answerResultText = findViewById(R.id.answerresult);
+        answer0Button = findViewById(R.id.answer0);
+        answer1Button = findViewById(R.id.answer1);
+        answer2Button = findViewById(R.id.answer2);
+        answer3Button = findViewById(R.id.answer3);
+        submitButton = findViewById(R.id.submit);
+        moreInfoButton = findViewById(R.id.moreinfo);
+        nextQuestionButton = findViewById(R.id.nextquestion);
+        buttonHolder = findViewById(R.id.buttonholder);
 
 
-                startNewGame();
+        startNewGame();
 
-                answer0Button.setOnClickListener(new View.OnClickListener() {
-                  @Override
-                  public void onClick(View v) {
-                    Question currentQuestion = getCurrentQuestion();
-                    playerAnswer = 0;
-                    setPlayerAnswer( currentQuestion, answer0Button,  0);
+        answer0Button.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+            Question currentQuestion = getCurrentQuestion();
+            playerAnswer = 0;
+            setPlayerAnswer(currentQuestion, answer0Button, 0);
 
-                    answer1Button.setText(currentQuestion.answerArray[1]);
-                    answer2Button.setText(currentQuestion.answerArray[2]);
-                    answer3Button.setText(currentQuestion.answerArray[3]);
-                  }
-                });
-
-                answer1Button.setOnClickListener(new View.OnClickListener() {
-                  @Override
-                  public void onClick(View v) {
-                    Question currentQuestion = getCurrentQuestion();
-
-                    playerAnswer = 1;
-                    setPlayerAnswer( currentQuestion, answer1Button,  1);
-
-                    answer0Button.setText(currentQuestion.answerArray[0]);
-                    answer2Button.setText(currentQuestion.answerArray[2]);
-                    answer3Button.setText(currentQuestion.answerArray[3]);
-                  }
-                });
-
-                answer2Button.setOnClickListener(new View.OnClickListener() {
-                  @Override
-                  public void onClick(View v) {
-                    Question currentQuestion = getCurrentQuestion();
-                    playerAnswer = 2;
-                    setPlayerAnswer( currentQuestion, answer2Button,  2);
-
-                    answer0Button.setText(currentQuestion.answerArray[0]);
-                    answer1Button.setText(currentQuestion.answerArray[1]);
-                    answer3Button.setText(currentQuestion.answerArray[3]);
-                  }
-                });
-
-                answer3Button.setOnClickListener(new View.OnClickListener() {
-                  @Override
-                  public void onClick(View v) {
-                    Question currentQuestion = getCurrentQuestion();
-                    playerAnswer = 3;
-                    setPlayerAnswer( currentQuestion, answer3Button,  3);
-
-                    answer0Button.setText(currentQuestion.answerArray[0]);
-                    answer1Button.setText(currentQuestion.answerArray[1]);
-                    answer2Button.setText(currentQuestion.answerArray[2]);
-                  }
-                });
-
-                submitButton.setOnClickListener(new View.OnClickListener() {
-                  @Override
-                  public void onClick(View view) {
-                    correctAnswerButton = findViewById(correctAnswerButtonId);
-                    Question currentQuestion = getCurrentQuestion();
-                    submitButton.setVisibility(View.GONE);
-                    if(currentQuestion.isCorrect()){
-                      answerResultText.setText("Correct!");
-                    } else {
-                      answerResultText.setText("Incorrect!");
-                    }
-                    correctAnswerButton.setText("\u2714" + currentQuestion.answerArray[currentQuestion.correctAnswer]);
-                    answerResultText.setVisibility(View.VISIBLE);
-                    buttonHolder.setVisibility(View.VISIBLE);
-                  }
-                });
-
-//                nextQuestionButton.setOnClickListener(new View.OnClickListener() {
-//                  @Override
-//                  public void onClick(View view) {
-//                    if(currentQuestionIndex < 5){
-//                      pickQuestion();
-//                      renderQuestion();
-//                    }
-//                  }
-//                });
-
-            }
+            answer1Button.setText(currentQuestion.answerArray[1]);
+            answer2Button.setText(currentQuestion.answerArray[2]);
+            answer3Button.setText(currentQuestion.answerArray[3]);
+            submitButton.setEnabled(true);
+          }
         });
+
+        answer1Button.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+            Question currentQuestion = getCurrentQuestion();
+
+            playerAnswer = 1;
+            setPlayerAnswer(currentQuestion, answer1Button, 1);
+
+            answer0Button.setText(currentQuestion.answerArray[0]);
+            answer2Button.setText(currentQuestion.answerArray[2]);
+            answer3Button.setText(currentQuestion.answerArray[3]);
+            submitButton.setEnabled(true);
+          }
+        });
+
+        answer2Button.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+            Question currentQuestion = getCurrentQuestion();
+            playerAnswer = 2;
+            setPlayerAnswer(currentQuestion, answer2Button, 2);
+
+            answer0Button.setText(currentQuestion.answerArray[0]);
+            answer1Button.setText(currentQuestion.answerArray[1]);
+            answer3Button.setText(currentQuestion.answerArray[3]);
+            submitButton.setEnabled(true);
+          }
+        });
+
+        answer3Button.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+            Question currentQuestion = getCurrentQuestion();
+            playerAnswer = 3;
+            setPlayerAnswer(currentQuestion, answer3Button, 3);
+
+            answer0Button.setText(currentQuestion.answerArray[0]);
+            answer1Button.setText(currentQuestion.answerArray[1]);
+            answer2Button.setText(currentQuestion.answerArray[2]);
+            submitButton.setEnabled(true);
+          }
+        });
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+            correctAnswerButton = findViewById(correctAnswerButtonId);
+            Question currentQuestion = getCurrentQuestion();
+            submitButton.setVisibility(View.GONE); //hide the submit button
+
+            if (currentQuestion.isCorrect()) {
+              countCorrect(true); //increment totalCorrectAnswers
+              String correct = "CORRECT!";
+              answerResultText.setText(correct);
+            } else {
+              String incorrect = "INCORRECT!";
+              answerResultText.setText(incorrect);
+            }
+            String text = "\u2714" + currentQuestion.answerArray[currentQuestion.correctAnswer];
+            correctAnswerButton.setText(text);
+            answerResultText.setVisibility(View.VISIBLE);
+            buttonHolder.setVisibility(View.VISIBLE);
+          }
+        });
+
+        nextQuestionButton.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+            if (currentQuestionIndex < 5) {
+              submitButton.setVisibility(View.VISIBLE);
+              answerResultText.setVisibility(View.GONE);
+              buttonHolder.setVisibility(View.GONE);
+              pickQuestion();
+              renderQuestion();
+            } else if(currentQuestionIndex == 5) {
+//              gameOver(totalCorrectAnswers, totalQuestions);
+            }
+          }
+        });
+
+      }
+    });
 
 
 
@@ -162,78 +179,102 @@ public class MainActivity extends AppCompatActivity {
            a  MainActivity.this.runOnUiThread to make the changes run on that thread.
 
          */
-        QuestionLookup questionLookup = new QuestionLookup();
-        questionLookup.createAllQuestions(()->{
-           gameQuestions =  questionLookup.getGameQuestions(); //(8)
-           MainActivity.this.runOnUiThread(() ->{
-               playButton.setEnabled(true); //(9)
-               playButton.setBackgroundColor(0xff339999);
-           });
-        });
+
+    QuestionLookup questionLookup = new QuestionLookup();
+    questionLookup.createAllQuestions(() -> {
+      gameQuestions = questionLookup.getGameQuestions(); //(8)
+      MainActivity.this.runOnUiThread(() -> {
+        playButton.setEnabled(true); //(9)
+        playButton.setBackgroundColor(0xff339999);
+      });
+    });
 
 
+  }
+
+  public void renderQuestion() {
+    Question question = getCurrentQuestion();
+    String imageText = question.imageName;
+    int imageId = this.getResources().getIdentifier(imageText, "drawable", this.getPackageName()); //found here: https://stackoverflow.com/questions/3476430/how-to-get-a-resource-id-with-a-known-resource-name
+    quoteImageView.setImageResource(imageId);
+    questionTextView.setText(question.questionText);
+    answer0Button.setText(question.answerArray[0]);
+    answer1Button.setText(question.answerArray[1]);
+    answer2Button.setText(question.answerArray[2]);
+    answer3Button.setText(question.answerArray[3]);
+
+  }
+
+  public void pickQuestion() {
+    Random random = new Random();
+    currentQuestionIndex += 1;
+    Question chosenQuestion = gameQuestions.get(currentQuestionIndex);
+    String correctAnswerButton = "answer" + chosenQuestion.correctAnswer;
+    correctAnswerButtonId = getResources().getIdentifier(correctAnswerButton, "id", getPackageName());
+  }
+
+  //
+  public Question getCurrentQuestion() {
+    Question question = null;
+    if (gameQuestions.size() > 0) {
+      question = gameQuestions.get(currentQuestionIndex);
     }
+    ;
+    return question;
+  }
 
-   public void renderQuestion(){
-        Question question = getCurrentQuestion();
-        String imageText = question.imageName;
-        int imageId = this.getResources().getIdentifier(imageText, "drawable", this.getPackageName()); //found here: https://stackoverflow.com/questions/3476430/how-to-get-a-resource-id-with-a-known-resource-name
-        quoteImageView.setImageResource(imageId);
-        questionTextView.setText(question.questionText);
-        answer0Button.setText(question.answerArray[0]);
-        answer1Button.setText(question.answerArray[1]);
-        answer2Button.setText(question.answerArray[2]);
-        answer3Button.setText(question.answerArray[3]);
+  public void setPlayerAnswer(Question currentQuestion, Button clickedButton, int answerInt) {
+    currentQuestion.playerAnswer = playerAnswer;
+    String text = "\uD83D\uDCAC" + currentQuestion.answerArray[answerInt];
+    clickedButton.setText(text);
+    // "\uD83D\uDCAC" **speech bubble java unicode
+  }
 
-   }
-
-   public Question pickQuestion(){
-      Random random = new Random();
-      currentQuestionIndex += 1;
-      Question chosenQuestion = gameQuestions.get(currentQuestionIndex);
-      String correctAnswerButton = "answer" + chosenQuestion.correctAnswer;
-      correctAnswerButtonId = getResources().getIdentifier(correctAnswerButton, "id", getPackageName());
-      return chosenQuestion;
-   }
-//
-   public Question getCurrentQuestion(){
-       Question question = null;
-       if(gameQuestions.size() > 0){
-            question = gameQuestions.get(currentQuestionIndex);
-//            gameQuestions.remove(0);
-        };
-       return question;
-   }
-
-   public void setPlayerAnswer(Question currentQuestion, Button clickedButton, int answerInt){
-      currentQuestion.playerAnswer = playerAnswer;
-      clickedButton.setText("\uD83D\uDCAC" + currentQuestion.answerArray[answerInt]);
-      // "\uD83D\uDCAC" **speech bubble java unicode
-   }
-
-    public int countCorrect(){
-        //this will count how many questions the player got correct and return the count
-        int numCorrect = 0;
-        return numCorrect;
+  public void countCorrect(Boolean result) {
+    //this will count how many questions the player got correct and return the count
+    if (result) {
+      totalCorrectAnswers += 1;
     }
+  }
 
-    public void startNewGame(){
-        /*
-        this method will contain all the logic that is needed when a new game starts
-        TODO: keep track of what questions is currently being displayed
-        */
+  public void startNewGame() {
+    totalCorrectAnswers = 0;
+    totalQuestions = gameQuestions.size();
 
-        pickQuestion();
-        totalCorrectAnswers = 0;
-        totalQuestions = gameQuestions.size();
+    pickQuestion();
+    renderQuestion();
 
-        renderQuestion();
-
-    }
+  }
 
 
-//    public String gameOver(int numberCorrect, int numberOfQuestions){
-//        // this will output some type of message  to the player that tells them if they won or lost
+//  public void gameOver(int numberCorrect, int numberOfQuestions) {
+//    // this will output some type of message  to the player that tells them if they won or lost
+//    String gameOverMessage;
+//    if (totalCorrectAnswers == totalQuestions) {
+//      gameOverMessage = "You got all " + totalQuestions + " right! YOU WIN!";
+//    } else {
+//      gameOverMessage = "You got " + totalCorrectAnswers + " right out of " + totalQuestions + ".  YOU LOSE! Better luck next time!";
 //    }
+//    AlertDialog.Builder gameOverDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+//    gameOverDialogBuilder.setCancelable(false);
+//    gameOverDialogBuilder.setTitle("Game Over!");
+//    gameOverDialogBuilder.setMessage(gameOverMessage);
+//
+//    gameOverDialogBuilder.setPositiveButton("Play Again!", new DialogInterface.OnClickListener() {
+//      @Override
+//      public void onClick(DialogInterface dialog, int which) {
+//        setContentView(R.layout.landing_page_main);
+//      }
+//    });
+//
+//    gameOverDialogBuilder.setNegativeButton("Leave Game!", new DialogInterface.OnClickListener() {
+//      @Override
+//      public void onClick(DialogInterface dialog, int which) {
+//        System.exit(0);
+//      }
+//    });
+//
+//    gameOverDialogBuilder.create().show();
+//  }
 
 }
